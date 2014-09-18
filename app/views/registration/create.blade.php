631f39adb8dbc965e935811c8ef8a30149db23e9
Jernej Pogacar <pogacar.jernej@gmail.com>
2014-09-18 13:21:41 +0000

    Fixed registration form placeholders

--- app/views/registration/create.blade.php
+++ app/views/registration/create.blade.php
@@ -5,28 +5,28 @@
         <!-- Username Form Input -->
         <div class="form-group">
             {{ Form::label('username', 'Username:') }}
-        	{{ Form::text('username', null, ['placeholder' => 'username']) }}
+        	{{ Form::text('username', null, ['placeholder' => 'Username']) }}
         	{{ $errors->first('username', '<span class="error">:message</span>') }}
         </div>
 
         <!-- Email Form Input -->
         <div class="form-group">
             {{ Form::label('email', 'Email:') }}
-        	{{ Form::email('email', null, ['placeholder' => 'password']) }}
+        	{{ Form::email('email', null, ['placeholder' => 'Email']) }}
         	{{ $errors->first('email', '<span class="error">:message</span>') }}
         </div>
 
         <!-- Password Form Input -->
         <div class="form-group">
             {{ Form::label('password', 'Password:') }}
-        	{{ Form::password('password', ['placeholder' => 'password']) }}
+        	{{ Form::password('password', ['placeholder' => 'Password']) }}
         	{{ $errors->first('password', '<span class="error">:message</span>') }}
         </div>
 
         <!-- Password_confirmed Form Input -->
         <div class="form-group">
             {{ Form::label('password_confirmation', 'Password Confirmation:') }}
-        	{{ Form::password('password_confirmation', ['placeholder' => 'password_confirmed']) }}
+        	{{ Form::password('password_confirmation', ['placeholder' => 'Password Confirmation']) }}
         </div>
 
         <!-- Register Form Input -->
