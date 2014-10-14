8c2c60a5b39f5159423c08eb88b1f8f6b352c0d1
Jernej Pogačar <pogacar.jernej@gmail.com>
2014-10-14 19:51:22 +0200

    Example .env file

---
+++ example.env.local.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'DB_HOST'		=> 'localhost',
+	'DB_NAME'		=> 'example',
+	'DB_USERNAME'	=> 'root',
+	'DB_PASSWORD'	=> 'root',
+	'APP_KEY'		=> 'laravel_app_key',
+	'MAIL_ADDRESS'	=> 'example@example.com',
+	'MAIL_NAME'		=> 'Admin',
+	'MAIL_USERNAME' => 'admin@example.com',
+	'MAIL_PASSWORD' => 'secret'
+];
